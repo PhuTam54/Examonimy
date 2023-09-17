8fb58f125feac80e9ffa38a7f11b650ff1b21293
PhuTam54 <phutamytb@gmail.com>
2023-09-17 10:05:53 +0700

    Add Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+//        \App\Models\User::factory(10)->create();
+//        \App\Models\Category::factory(10)->create();
+//        \App\Models\Product::factory(100)->create();
+//        \App\Models\Order::factory(10)->create();
+//        \App\Models\OrderDetails::factory(10)->create();
     }
 }
