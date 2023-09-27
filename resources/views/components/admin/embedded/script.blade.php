4dcb314d25402ba108841b6d6cf94bc6b086edbd
PhuTam54 <phutamytb@gmail.com>
2023-09-27 10:48:36 +0700

    Add Admin

---
+++ resources/views/components/admin/embedded/script.blade.php
@@ -0,0 +1,34 @@
+<!-- jQuery -->
+<script src="js/admin-js/plugins/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="js/admin-js/plugins/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="js/admin-js/plugins/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="js/admin-js/plugins/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="js/admin-js/plugins/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="js/admin-js/plugins/jquery.vmap.min.js"></script>
+<script src="js/admin-js/plugins/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="js/admin-js/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="js/admin-js/plugins/moment/moment.min.js"></script>
+<script src="js/admin-js/plugins/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="js/admin-js/plugins/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="js/admin-js/plugins/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="js/admin-js/plugins/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="js/admin-js/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="js/admin-js/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="js/admin-js/dist/js/pages/dashboard.js"></script>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield("title", "AdminLTE 3 | Dashboard")</title>
+    <meta name="description" content="Admin Template">
+    <meta name="keywords" content="Ogani, unica, creative, html">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <base href="/">
+
+    @yield("before_css")
+    @include('components.admin.embedded.head')
+    @yield("after_css")
+</head>
+<body>
+@include("components.admin.dashboard.header")
+<main>
+    @yield("main")
+</main>
+@include("components.admin.dashboard.sidebar")
+@include("components.admin.dashboard.footer")
+
+@yield("before_js")
+@include('components.admin.embedded.script')
+@yield("after_js")
+</body>
+</html>
